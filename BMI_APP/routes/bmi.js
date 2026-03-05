2be5a2a85375f079effb0f90d192b40114de4f38
Berit Haugen <movies.tv.and.music@gmail.com>
2026-03-04 19:29:53 -0600

    End of class

--- BMI_APP/routes/bmi.js
+++ BMI_APP/routes/bmi.js
@@ -7,5 +7,21 @@ router.get('/', function(req, res, next) {
     res.render('home')
 })
 
+router.get('/calculate', function(req, res, next) {
+    console.log('Calcuating...')
+    // How to get the data from the request?
+    const formData = req.query
+    console.log(formData)
+    // formData is an object e.g. {height: '6'}
+    const height = Number(formData.height) 
+    const weight = Number(formData.weight)
+    // You could definately do validation here
+
+    // Math
+    // Correct the math here!
+    const bmiResult = height * 2 / weight
+    res.render('results',{bmi: bmiResult} )
+})
+
 // last line of the file
 module.exports = router
